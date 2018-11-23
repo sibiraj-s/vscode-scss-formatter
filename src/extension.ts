9a6fd4056f904a090c730566afaf7fade9229381
Sibiraj <dev.sibiraj@outlook.com>
2018-11-23 08:44:59 +0530

    perf: handle extension registration efficiently

--- src/extension.ts
+++ src/extension.ts
@@ -1,49 +1,18 @@
 'use strict';
 
-import {
-  commands, Disposable, ExtensionContext,
-  languages, window, workspace
-} from 'vscode';
+import { ExtensionContext, languages } from 'vscode';
 
+import { ACTIVATION_COMMAND } from './commandManager';
 import { registerErrorHandlerDisposables, setupErrorHandler } from './errorHandler';
-import { SCSSFormatter } from './ScssFormatProvider';
+import { SCSSFormatter } from './scssFormatterProvider';
 import { languageSelector } from './utils';
-
-let formatterHandler: undefined | Disposable;
-
-// dispose formatter
-function disposeHandlers() {
-  if (formatterHandler) {
-    formatterHandler.dispose();
-  }
-  formatterHandler = undefined;
-}
-
-// register formatter
-function registerFormatter() {
-  disposeHandlers();
-  const scssFormatter = new SCSSFormatter();
-  formatterHandler = languages.registerDocumentFormattingEditProvider(
-    languageSelector, scssFormatter
-  );
-}
 
 // method is called when extension is activated
 export function activate(context: ExtensionContext) {
-  registerFormatter();
+  const scssFormatter = new SCSSFormatter();
 
-  const disposable = commands.registerCommand('scss-formatter.activate', () => {
-    window.showInformationMessage('SCSS Formatter Activated');
-  });
-
-  context.subscriptions.push(
-    workspace.onDidChangeWorkspaceFolders(registerFormatter),
-    { dispose: disposeHandlers },
-    setupErrorHandler(),
-    disposable,
-    ...registerErrorHandlerDisposables()
-  );
+  context.subscriptions.push(languages.registerDocumentFormattingEditProvider(languageSelector, scssFormatter));
+  context.subscriptions.push(ACTIVATION_COMMAND);
+  context.subscriptions.push(setupErrorHandler());
+  context.subscriptions.push(...registerErrorHandlerDisposables());
 }
-
-// method is called when extension is deactivated
-export function deactivate() { } // tslint:disable-line:no-empty
