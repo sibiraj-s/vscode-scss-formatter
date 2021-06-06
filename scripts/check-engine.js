c8af0b623d830ba3d1c70d9aa0f7b1d5c92774e7
sibiraj-s <dev.sibiraj@outlook.com>
2021-06-06 13:11:46 +0530

    chore: check for manifest version mismatch

--- scripts/check-engine.js
+++ scripts/check-engine.js
@@ -1,9 +1,15 @@
 const pkg = require('../package.json');
+const pkgLock = require('../package-lock.json');
 
 const vscodeVersion = parseFloat(pkg.engines.vscode.substring(1));
 const typesVscodeVersion = parseFloat(pkg.devDependencies['@types/vscode'].substring(1));
 
 if (vscodeVersion !== typesVscodeVersion) {
   console.error('Err: Vscode Engines version and \'@types/vscode\' version doesn\'t match');
+  process.exit(1);
+}
+
+if (pkg.version !== pkgLock.version) {
+  console.error('Err: Manifest and lock file version mismatch');
   process.exit(1);
 }
