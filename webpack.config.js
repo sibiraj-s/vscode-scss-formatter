0016c0db0c9e580a0c450a1693cb132692ddedcd
sibiraj-s <dev.sibiraj@outlook.com>
2021-10-16 14:49:23 +0530

    fix: process prettier as external module

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ const extensionConfig = {
   externals: {
     vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vsceignore file
+    prettier: 'commonjs prettier',
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
