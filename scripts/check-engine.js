fad7a8e953c6a1fb6857eab3eecef3dc31bfd9a2
Sibiraj <dev.sibiraj@outlook.com>
2022-02-03 09:15:43 +0530

    chore: update vscode version check script

--- scripts/check-engine.js
+++ scripts/check-engine.js
@@ -3,8 +3,8 @@ const color = require('picocolors');
 const pkg = require('../package.json');
 const pkgLock = require('../package-lock.json');
 
-const vscodeVersion = parseFloat(pkg.engines.vscode.substring(1));
-const typesVscodeVersion = parseFloat(pkg.devDependencies['@types/vscode'].substring(1));
+const vscodeVersion = pkg.engines.vscode.substring(1);
+const typesVscodeVersion = pkg.devDependencies['@types/vscode'].substring(1);
 
 if (vscodeVersion !== typesVscodeVersion) {
   const msg = color.red('Err: Version specified in engines.vscode and \'@types/vscode\' doesn\'t match');
